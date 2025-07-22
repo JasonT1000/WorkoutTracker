da6d56510e687a8152013db24dd1fe59ffd1d249
JasonTollison <jason_tollison@hotmail.com>
2025-07-22 15:19:29 +1200

    Reset to blank project. Setup basic navigation and themeview

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,60 +1,15 @@
-import { Image, StyleSheet, View } from 'react-native';
+import { Text, View } from "react-native";
 
-import SvgComponent from '@/components/SvgImage';
-
-export default function HomeScreen() {
+export default function Index() {
   return (
-    <View style={styles.imagesContainer}>
-      <Image
-        style={styles.imageTestTwo}
-        width={250}
-        height={250}
-        source={require('../../assets/images/Musculature_FrontBack_Foreground.png')}
-      />
-
-      <View style={styles.imageTestOne}>
-        <SvgComponent />
-      </View>
-
-      <Image
-        style={styles.imageTestTwo}
-        width={250}
-        height={250}
-        source={require('../../assets/images/Musculature_FrontBack_Background.png')}
-      />
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  imagesContainer: {
-
-  },
-  // titleContainer: {
-  //   flexDirection: 'row',
-  //   alignItems: 'center',
-  //   gap: 8,
-  // },
-  // stepContainer: {
-  //   gap: 8,
-  //   marginBottom: 8,
-  // },
-  // reactLogo: {
-  //   height: 178,
-  //   width: 290,
-  //   bottom: 0,
-  //   left: 0,
-  //   position: 'absolute',
-  // },
-  imageTestOne: {
-    position: 'absolute',
-    left: 50,
-    top: 30,
-  },
-  imageTestTwo: {
-    position: 'absolute',
-    left: 50,
-    top: 30,
-    resizeMode: 'contain',
-  }
-});
